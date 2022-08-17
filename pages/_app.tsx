14eba62361843cdc30270dfe8d732f3edb0b07e7
niv54 <nivznivz0@gmail.com>
2022-08-17 05:07:26 +0300

    add emotion cache and theming support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,43 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import * as React from "react";
+import Head from "next/head";
+import { createTheme } from "@mui/material/styles";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import { green } from "@mui/material/colors";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { createEmotionCache } from "../styles/setup/createEmotionCache";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: green.A700
+      },
+      secondary: {
+        main: "#ff6666"
+      }
+    }
+  });
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
