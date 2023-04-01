8c24284f844edf2d5499973fe91dacb079e76538
nivznivz0@gmail.com <nivznivz0@gmail.com>
2023-04-01 21:36:24 +0300

    remove redundant file

--- utils/search.ts
+++
@@ -1,4 +0,0 @@
-export const branchesFilter = (search: string, filterParams: string[]) => {
-  const keywords = search.split(" ").map(keyword => keyword.trim());
-  return keywords.every(keyword => filterParams.some(filterParam => filterParam.includes(keyword)));
-};
