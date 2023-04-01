da856421e65678d33d60caa9b017f3fc596c79bd
nivznivz0@gmail.com <nivznivz0@gmail.com>
2023-04-01 21:35:50 +0300

    split topbar component to separate files

--- components/layout/topbar/index.tsx
+++ components/layout/topbar/index.tsx
@@ -1,51 +1,12 @@
-import styled from "@emotion/styled";
 import Home from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
-import InputBase from "@mui/material/InputBase";
-import Toolbar from "@mui/material/Toolbar";
 
-import { useSearchStore } from "../../store/searchStore";
+import { useSearchStore } from "../../../store/searchStore";
 
-const Search = styled.div`
-  position: relative;
-  width: fit-content;
-
-  border-radius: 15px;
-
-  background-color: white;
-`;
-
-const SearchIconWrapper = styled.div`
-  position: absolute;
-  height: 100%;
-  float: right;
-  z-index: 1;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  pointer-events: none;
-`;
-
-const StyledToolbar = styled(Toolbar)`
-  background-color: #003a68;
-`;
-const StyledInputBase = styled(InputBase)`
-  width: 100px;
-  :focus-within {
-    width: 200px;
-  }
-
-  transition: all 0.5s;
-
-  input {
-    padding-right: calc(1em + 10px);
-  }
-`;
+import { Search, SearchIconWrapper, StyledInputBase, StyledToolbar } from "./topbar.styled";
 
 export const Topbar = () => {
   const { search, setSearch } = useSearchStore();
---
+++ components/layout/topbar/topbar.styled.tsx
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+import InputBase from "@mui/material/InputBase";
+import Toolbar from "@mui/material/Toolbar";
+
+export const Search = styled.div`
+  position: relative;
+  width: fit-content;
+
+  border-radius: 15px;
+
+  background-color: white;
+`;
+
+export const SearchIconWrapper = styled.div`
+  position: absolute;
+  height: 100%;
+  float: right;
+  z-index: 1;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  pointer-events: none;
+`;
+
+export const StyledToolbar = styled(Toolbar)`
+  background-color: #003a68;
+`;
+
+export const StyledInputBase = styled(InputBase)`
+  width: 100px;
+  :focus-within {
+    width: 200px;
+  }
+
+  transition: all 0.5s;
+
+  input {
+    padding-right: calc(1em + 10px);
+  }
+`;
