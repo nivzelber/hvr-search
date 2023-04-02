d4638d630e1c6083cdb850909173abde2be50c0e
nivznivz0@gmail.com <nivznivz0@gmail.com>
2023-04-03 00:46:04 +0300

    prune search text before searching

--- utils/array/filter.ts
+++ utils/array/filter.ts
@@ -1,11 +1,15 @@
+const pruneAsSearchText = (text: string) => text.toLowerCase().replaceAll(/[^א-תa-zA-Z0-9 ]/g, "");
+
 export const branchesFilter =
   <TBranch extends Record<string, any>>(search: string, filterParams: (keyof TBranch)[]) =>
   (branch: TBranch) => {
-    const keywords = search.split(" ").map(keyword => keyword.trim());
+    const keywords = pruneAsSearchText(search)
+      .split(" ")
+      .map(keyword => keyword.trim());
 
     const filterValues = Object.entries(branch)
       .filter(([key, _value]) => filterParams.includes(key))
-      .map(([_key, value]) => value);
+      .map(([_key, value]) => pruneAsSearchText(value));
 
     return keywords.every(keyword =>
       filterValues.some(filterValue => filterValue.includes(keyword))
