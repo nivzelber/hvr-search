ce9c00f9c304e6a8d8f62be86e50d436a37bf2d0
nivznivz0@gmail.com <nivznivz0@gmail.com>
2023-04-05 18:14:07 +0300

    minor search optimizations

--- utils/array/filter.ts
+++ utils/array/filter.ts
@@ -1,12 +1,20 @@
 const pruneAsSearchText = (text: string) => text.toLowerCase().replaceAll(/[^א-תa-zA-Z0-9 ]/g, "");
 
-export const branchesFilter =
-  <TBranch extends Record<string, any>>(search: string, filterParams: (keyof TBranch)[]) =>
-  (branch: TBranch) => {
-    const keywords = pruneAsSearchText(search)
-      .split(" ")
-      .map(keyword => keyword.trim());
+export const branchesFilter = <TBranch extends Record<string, any>>(
+  search: string,
+  filterParams: (keyof TBranch)[]
+) => {
+  const keywords = pruneAsSearchText(search)
+    .split(" ")
+    .map(keyword => keyword.trim());
 
+  return (branch: TBranch) => {
+    /**
+     * There are ways to optimize this filter, as it does basically the same thing
+     * every new search. A cache per (branch + filterParams) can be added.
+     * However, after testing, the optimization is not needed as this is not the
+     * "heavy" part of the operation
+     */
     const filterValues = Object.entries(branch)
       .filter(([key, _value]) => filterParams.includes(key))
       .map(([_key, value]) => pruneAsSearchText(value));
@@ -15,3 +23,4 @@ export const branchesFilter =
       filterValues.some(filterValue => filterValue.includes(keyword))
     );
   };
+};
