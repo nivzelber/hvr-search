1f06bda8117118e49fc996d787a6cb7b388bd817
nivznivz0@gmail.com <nivznivz0@gmail.com>
2023-04-14 16:47:37 +0300

    use pwa in production only

--- next.config.js
+++ next.config.js
@@ -8,4 +8,4 @@ const withPWA = require("next-pwa")({
   dest: "public"
 });
 
-module.exports = withPWA(nextConfig);
+module.exports = process.env.NODE_ENV !== "development" ? withPWA(nextConfig) : nextConfig;
