51688caecc94870831378ca11db41ddd417147d2
Niv Zelber <nivznivz0@gmail.com>
2023-09-14 02:24:17 +0300

    add search log

--- hooks/useBranchesSearch.ts
+++ hooks/useBranchesSearch.ts
@@ -1,6 +1,8 @@
-import { useMemo } from "react";
+import { Logger } from "next-axiom";
+import { useEffect, useMemo } from "react";
 import { useDebounce } from "use-debounce";
 
+import * as messages from "../logger/messages";
 import { useSearchStore } from "../store/searchStore";
 import { branchesFilter } from "../utils/array/filter";
 
@@ -18,6 +20,10 @@ export const useBranchesSearch = <TBranch extends Record<string, any>>({
   const search = useSearchStore(state => state.search);
 
   const [debouncedSearch] = useDebounce(search, debounceTimeout);
+  useEffect(() => {
+    const logger = new Logger();
+    logger.info(messages.SEARCH, { search: debouncedSearch, pathname: window.location.pathname });
+  }, [debouncedSearch]);
 
   const filteredBranches = useMemo(
     () => branches.filter(branchesFilter(debouncedSearch, filterParams)),
