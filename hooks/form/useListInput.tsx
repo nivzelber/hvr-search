b0b37729c11a8775f09a1085c16c8d037624ec0a
Niv Zelber <nivznivz0@gmail.com>
2023-09-13 20:33:20 +0300

    lint and fix import

--- hooks/form/useListInput.tsx
+++ hooks/form/useListInput.tsx
@@ -1,26 +1,27 @@
 import { Autocomplete, TextField } from "@mui/material";
-import { uniq } from "lodash";
+import uniq from "lodash/uniq";
 import { useMemo, useState } from "react";
 
-export const useListInput = <Item extends Record<string, string>>(items: Item[], key: keyof Item, label: string ) => {
-  const options = useMemo(
-        () => uniq(items.map(item => item[key])).sort(),
-        [items, key]
-    );
+export const useListInput = <Item extends Record<string, string>>(
+  items: Item[],
+  key: keyof Item,
+  label: string
+) => {
+  const options = useMemo(() => uniq(items.map(item => item[key])).sort(), [items, key]);
 
-    const [ value, setValue ] = useState<string | null>(null);
-    const handleCategoryChange = (_: unknown, newValue: string | null) => {
-      setValue(newValue);
-    };
+  const [value, setValue] = useState<string | null>(null);
+  const handleCategoryChange = (_: unknown, newValue: string | null) => {
+    setValue(newValue);
+  };
 
-    const Input = (
-         <Autocomplete
-            onChange={handleCategoryChange}
-            options={options}
-            value={value}
-            renderInput={(props) => <TextField {...props} label={label} sx={{padding:"0.5em"}}/>}
-        />
-    )
+  const Input = (
+    <Autocomplete
+      onChange={handleCategoryChange}
+      options={options}
+      value={value}
+      renderInput={props => <TextField {...props} label={label} sx={{ padding: "0.5em" }} />}
+    />
+  );
 
-    return [ value, Input ];
+  return [value, Input];
 };
